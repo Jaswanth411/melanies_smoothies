98e56d2aa8c6c10dd10da1198ff04fade6de9b0e
Jaswanth411 <jaswanth41100@gmail.com>
2024-07-06 23:28:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,9 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('Fruit_name'))
 #st.dataframe(data = my_dataframe, use_container_width = True)
 
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 
 
